e36da779b8e23f932d8221029a21550011d0f0ae
Heinz Marti <hem@zuehlke.com>
2017-05-24 16:41:29 +0200

    add test for loading executable

--- src/test/java/org/web3j/mavenplugin/solidity/SolCTest.java
+++ src/test/java/org/web3j/mavenplugin/solidity/SolCTest.java
@@ -1,6 +1,20 @@
 package org.web3j.mavenplugin.solidity;
 
-//TODO
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class SolCTest {
+
+    @Test
+    public void loadExecutable(){
+        SolC solC = new SolC();
+
+        assertTrue(solC.getWorkingDirectory().exists());
+        assertFalse(solC.getCanonicalPath().isEmpty());
+        assertFalse(solC.getCanonicalWorkingDirectory().isEmpty());
+        assertTrue(solC.getCanonicalPath().contains(solC.getCanonicalWorkingDirectory()));
+    }
 
 }
