a52cb582fa910c457302943197e77897f14d44e5
Heinz Marti <github@h2m.ch>
2017-07-18 11:04:06 +0200

    revert to xcode8.3, new access rights for SolC

--- src/main/java/org/web3j/mavenplugin/solidity/SolC.java
+++ src/main/java/org/web3j/mavenplugin/solidity/SolC.java
@@ -35,6 +35,9 @@ public class SolC {
 
     private void initBundled() throws IOException {
         File tmpDir = new File(System.getProperty("java.io.tmpdir"), "solc");
+        tmpDir.setReadable(true);
+        tmpDir.setWritable(true);
+        tmpDir.setExecutable(true);
         tmpDir.mkdirs();
 
         InputStream is = getClass().getResourceAsStream("/native/" + getOS() + "/solc/file.list");
