4b6b2e912f6dd3fe94b1955ba9b515374554c217
Heinz Marti <github@h2m.ch>
2017-11-10 12:32:21 +0100

    testcase for https://github.com/web3j/web3j-maven-plugin/issues/9

---
+++ src/test/java/org/web3j/mavenplugin/IssueITest.java
@@ -0,0 +1,51 @@
+package org.web3j.mavenplugin;
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.apache.maven.plugin.testing.resources.TestResources;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class IssueITest {
+
+    @Rule
+    public MojoRule mojoRule = new MojoRule();
+
+    @Rule
+    public TestResources resources = new TestResources();
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Test
+    public void issue9_library() throws Exception {
+        File pom = new File(resources.getBasedir("issue"), "issue9.pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        JavaClassGeneratorMojo mojo = (JavaClassGeneratorMojo) mojoRule.lookupMojo("generate-sources", pom);
+        assertNotNull(mojo);
+
+        mojo.sourceDestination = testFolder.getRoot().getPath();
+        mojo.execute();
+
+        Path path = Paths.get(mojo.sourceDestination);
+
+        List<Path> files = Files.find(path, 99, (p, bfa) -> bfa.isRegularFile()).collect(Collectors.toList());
+        assertThat("ConvertLib is created", files.size(), is(1));
+        assertThat(files.get(0).getFileName().toString(), is("ConvertLib.java"));
+    }
+
+}
---
+++ src/test/resources/issue-09.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.2;
+
+
+library ConvertLib {
+    function convert(uint amount, uint conversionRate) pure public returns (uint convertedAmount)
+    {
+        return amount * conversionRate;
+    }
+}
