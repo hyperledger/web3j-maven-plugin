1b39f4a538d23ac829044789f6f271cf0f9eceb8
Heinz Marti <github@h2m.ch>
2018-06-22 14:39:24 +0200

    revert solc version

--- src/main/java/org/web3j/mavenplugin/solidity/SolidityCompiler.java
+++ src/main/java/org/web3j/mavenplugin/solidity/SolidityCompiler.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -85,12 +86,12 @@ public class SolidityCompiler {
 
         List<String> commandParts = prepareCommandOptions(canonicalSolCPath, rootDirectory, sources, options);
 
-/*        LOG.warn("Issue with travis [canonicalSolCPath=" + canonicalSolCPath
+        LOG.warn("Issue with travis [canonicalSolCPath=" + canonicalSolCPath
                 + ",commandParts=" + commandParts.stream().collect(Collectors.joining(","))
                 + "],getWorkingDirectory=" + solc.getWorkingDirectory().getAbsolutePath() + "]");
         Files.newDirectoryStream(Paths.get(solc.getCanonicalWorkingDirectory()))
                 .forEach(file -> LOG.warn(file.toString()));
-*/
+
         ProcessBuilder processBuilder = new ProcessBuilder(commandParts)
                 .directory(solc.getWorkingDirectory());
         processBuilder
