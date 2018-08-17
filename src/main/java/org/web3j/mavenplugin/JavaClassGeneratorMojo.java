d2f3ed7781506321f5024129e773c073f1c5f3e1
Ben Wheeler <bwheeler@thoughtworks.com>
2018-08-17 13:22:04 +0100

    Fix for #19 script engine classloading issue

--- src/main/java/org/web3j/mavenplugin/JavaClassGeneratorMojo.java
+++ src/main/java/org/web3j/mavenplugin/JavaClassGeneratorMojo.java
@@ -91,7 +91,7 @@ public class JavaClassGeneratorMojo extends AbstractMojo {
 
     private Map<String, Map<String, String>> extractContracts(String result) throws MojoExecutionException {
         try {
-            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            ScriptEngine engine = new ScriptEngineManager(null).getEngineByName("nashorn");
             String script = "JSON.parse(JSON.stringify(" + result + "))";
             Map<String, Object> json = (Map<String, Object>) engine.eval(script);
             Map<String, Map<String, String>> contracts = (Map<String, Map<String, String>>) json.get("contracts");
