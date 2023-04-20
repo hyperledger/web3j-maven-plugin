a64cc2cc8ff23f8f98bf7cbd24c3bec4de905fb7
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-04-20 14:31:52 +0200

    package issue for abi & bin fixed and unit test added

---
+++ src/test/resources/issue-106/sample.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.1 < 0.9.0;
+
+
+library ConvertLib {
+    function convert(uint amount, uint conversionRate) pure public returns (uint convertedAmount)
+    {
+        return amount * conversionRate;
+    }
+}
